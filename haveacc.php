33c18c470cec93236a00d4ea9670240d2afe10a1
sandwichie <124646039+sandwichie@users.noreply.github.com>
2024-11-11 17:52:53 +0800

    Update haveacc.php

--- haveacc.php
+++ haveacc.php
@@ -61,248 +61,6 @@ if (isset($_POST['login'])) {
         box-sizing: border-box;
         font-family: "Playfair Display", serif;
     };
-    /* =======================================index==================================================================*/
-    .navbar{
-        display: flex;
-        align-items: center;
-        padding: 20px;
-    }
-
-    .far fa-eye{
-        margin-right: -30px; 
-        cursor: pointer;
-    }
-
-    nav{
-        flex: 1;
-        text-align: right;
-    }
-
-    nav ul{
-        display: inline-block;
-        list-style: none;
-    }
-
-    nav ul li {
-        display: inline-block;
-        margin-right: 20px;
-    }
-    a{ 
-        text-decoration: none;
-        color: #bc9c22;
-    }
-    p{
-        color: #555;
-    }
-
-    .container{
-        max-width: 1300px;
-        margin: auto;
-        padding-left: 25px;
-        padding-right: 25px;
-    }
-    .row{
-        display: flex;
-        align-items: center;
-        flex-wrap: wrap;
-        justify-content: space-around;
-    }
-    .col-2{
-        flex-basis: 50%;    
-        min-width: 300px;
-    }
-    .col-2 img{
-        max-width: 100%;
-        padding: 50px 0;
-    }
-    .col-2 h1{
-        font-size: 50px;
-        line-height: 60px;
-        margin: 25px 0;
-    }
-    .btn{
-        display: inline-block;
-        background:#bc9c22;
-        color: #fff;
-        padding: 8px 30px;
-        margin: 30px 0;
-        border-radius: 30px;
-        transition: background 0.5s;
-    }
-
-    .btn:hover{
-        background: #77430a;
-    }
-    .header{
-        background: radial-gradient(#fff,#F9F9D5);
-        /*background: #313131 url(images/bgLogin.png) left center/contain no-repeat padding-box; */
-        
-    }
-
-    .categories{
-        
-        margin: 70px auto;
-        max-width: 2224px;
-        padding: 30px 0;
-        width: 90%;
-        margin-top: 80px;
-    }
-    .row {
-        max-width: 1000px;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
-        margin-left: auto;
-    }
-    .col-6 {
-        flex-basis: 25%;
-        min-width: 25px;
-        margin-bottom: 30px;
-        align-items: center;
-        opacity: 1; 
-    
-
-    }
-    .col-6 img{
-        width: 100%;
-        align-items: center;
-        height: 100%;
-        border-radius: 5px;
-        
-        opacity: 1;
-
-
-    }
-    .list{
-        display:flex;
-        text-align: left;
-        text-transform: uppercase;
-        margin-left: 20;
-        align-items: center;
-        
-    }
-
-    /* Gallery Section */
-    .gallery {
-        display: flex;
-        flex-wrap: wrap;
-        gap: 10px; /* Spacing between images */
-        justify-content: center; /* Centers the gallery items horizontally */
-        max-width: 600px; /* Width limit to match the design */
-        margin: 20px auto; /* Center align and add top/bottom spacing */
-    }
-    .content {
-        flex: 1; /* Makes the content section take the remaining space */
-        display: flex;
-        flex-direction: column;
-        gap: 20px;
-    }
-
-
-    .gallery img {
-        width: 100%;
-        max-width: 280px; /* Sets a max width for each image */
-        border-radius: 8px; /* Rounds the corners of each image */
-    }
-
-    .col-3{
-        max-width: 1300px;
-        display: block;
-        background-color: #282727;
-        color: #FFFFFF;
-        overflow: auto;
-        
-        display: flex;
-                flex-direction: column;
-                align-items: center;
-                margin-top: 50px;
-                padding: 30px;
-                border-radius: 10px;
-                
-                box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
-                width: 500px;
-                margin-bottom: 60px;
-                margin-left: 1000px;
-                text-align: center;
-    }
-    .abtpic{
-        max-width: 1000px;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
-        margin-left: auto;
-        width: 100%;
-        align-items: left;
-        height: 100%;
-        border-radius: 5px;
-    }
-    .submit{
-        background-color: #B99470
-    }
-    .col-4{
-        margin-top: 20px;
-        border: 5px solid #282727;
-        box-shadow: 5px 5px 10px 1px #000000;
-        line-height: 2;
-        margin-right: 10px;
-        display: flex;
-                flex-direction: column;
-                align-items: center;
-                margin-top: 30px;
-                padding: 30px;
-                border-radius: 1px;
-        display: flex;
-                align-items: center;
-                margin-bottom: 10px;
-                color: #333;
-                text-decoration: none;width: 500px;
-        margin-left: 1000px;
-        margin-bottom: 30px;
-    
-    }.col-4 {
-        margin-top: 20px;
-        bottom: 20px;
-        left: 20px;
-        border: 5px solid #282727;
-        box-shadow: 5px 5px 10px 1px #000000;
-        line-height: 2;
-        padding: 30px;
-        width: 500px;
-        margin-left: 1000px;
-        margin-bottom: 30px;
-        border-radius: 1px;
-        display: flex;
-        flex-direction: column;
-        align-items: left; 
-        /*background-color: #F9F9D5;*/
-    }
-
-    .contact-item {
-        display: flex;
-        align-items: center;
-        margin-bottom: 10px;
-    }
-
-    .contact-item img {
-        margin-right: 10px;
-        
-    }
-
-    .contact-item a {
-        color: #333;
-        text-decoration: none;
-    }
-
-    .contact-item a:hover {
-        text-decoration: underline;
-        text-align: center;
-    }
-
-    .loc{
-        display: block;
-        background-color: #7E7E7E;
-        color: #2E2E31;  padding-left: 20px; padding-bottom: 20px;
-        }
 
     /* =======================================================LOGIN========================================*/
 
@@ -311,7 +69,6 @@ if (isset($_POST['login'])) {
         margin: 0;
         width: 100%;
         padding: 0;
-        background: radial-gradient(#fff,#FFF9E6);
     }
 
     .containerlogin {
@@ -319,6 +76,7 @@ if (isset($_POST['login'])) {
         justify-content: center;
         align-items: center;
         height: 100vh; 
+        background-image: url(/SND/Assets/bgLogin.png);
     }
 
     .login-form {
@@ -406,18 +164,29 @@ if (isset($_POST['login'])) {
     .buttons {
         display: flex;
         justify-content: space-between;
-        margin-top: 20px;
+        margin-top: 30px;
+        padding: 15px;
+        position: relative;
     }
 
+    .buttons .signupbtn:hover {
+        background-color: #bc9c22;
+    }
    
+    .buttons .cancelbtn:hover {
+        background-color: #2E2E31;
+    }
+
     .buttons .cancelbtn,
     .buttons .signupbtn {
         width: 48%;
-        padding: 12px;
+        padding: 10px;
         font-size: 16px;
         border: none;
         border-radius: 5px;
         cursor: pointer;
+        transition: all 0.3s ease; /* Smooth transition */
+        border: 1px solid #000000;
     }
 
     .buttons .cancelbtn {
@@ -430,64 +199,41 @@ if (isset($_POST['login'])) {
         color: #333;
     }
 
-    .social-buttons {
-        display: flex;
-        justify-content: space-around;
-        margin-top: 20px;
-        gap: 15px;
-    }
-
-    .social-buttons button {
-        display: flex;
-        align-items: center;
-        background-color: #E6E9F5;
-        color: #000;
-        border: none;
-        padding: 10px;
-        font-size: 14px;
-        cursor: pointer;
-        border-radius: 5px;
-        width: 48%;
-    }
-
-    .social-buttons button:hover {
-        opacity: 0.5;
-    }
-
-    .social-buttons button img {
-        margin-right: 10px;
-    }
-
-    .social-buttons button.facebook {
-        background-color: #4267B2;
-        color: white;
-    }
-
-    .social-buttons button.google {
-        background-color: #fff;
-        border: 1px solid #ccc;
-    }
-
     .login-form img {
         display: block;
         margin-left: auto;
         margin-right: auto;
         margin-bottom: 10px;
-        width: 25%; /* Adjust as needed */
+        width: 25%; 
+    }
+
+    .forgot-password {
+        color: #bc9c22;
+        text-decoration: none;
+        margin-bottom: 50px;
+        padding: 10px;
+
+    }
+
+    .forgot-password:hover {
+        text-decoration: underline;
+    }
+
+    .signup:hover {
+        text-decoration: underline;
     }
 
     </style>
 </head>
 <body>
 
-      </div>
       <div class="containerlogin">
         <div class="login-form">
         <form action="" method="POST">
             <img src="Assets/sndlogo-wShadow.png" alt="logo"/>
             <h1>Login</h1>
             <p>New to S&D Shop? 
-                <a href="sndLandingpage.php">Sign Up</a>
+                <a href="SignUp.php" class="signup">Sign Up</a>
             </p>
 
             <label for="email">Email</label>
@@ -500,28 +246,17 @@ if (isset($_POST['login'])) {
                 </div>
             <label>
             <!--<input type="checkbox" checked="checked" name="remember" style="margin-bottom: 15px"/> Remember me </label> -->
-            <a href="forgot_pass.php" class="forgot-password">  Forgot Password<br></a>
+            <a href="forgot_pass.php" class="forgot-password">Forgot Password<br></a>
          <!--  <p>
             <Br>By creating an account you agree to our
             <a href="#">Terms & Privacy</a>.
           </p> -->
 
-        <div class="buttons">
-            <button type="button" class="cancelbtn" onclick="window.location.href='sndLandingpage.php';">Cancel</button>
-            <button type="submit" name ="login" class="signupbtn">Login</button>
-        </div>
+            <div class="buttons">
+                <button type="button" class="cancelbtn" onclick="window.location.href='sndLandingpage.php';">Cancel</button>
+                <button type="submit" name ="login" class="signupbtn">Login</button>
+            </div>
         </form>
-
-        <div class="social-buttons">
-            <button class="google">
-                <img src="images/google-icon.png" alt="Google" width="20">
-                Continue with Google
-            </button>
-            <button class="facebook">
-                <img src="images/fb-icon.png" alt="Facebook" width="20">
-                Continue with Facebook
-            </button>
-        </div>
       </div>
       
     </div>
